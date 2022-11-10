5332f9cc56cbc07cb1ee06f463d67a623c6ebd5a
kjkandrea <kjkandrea@gmail.com>
2022-11-10 17:28:07 +0900

    feat: add getCenterPosition

--- src/graphic/ColorfulShapeGraphic.ts
+++ src/graphic/ColorfulShapeGraphic.ts
@@ -1,3 +1,5 @@
+type VertexXY = [number, number];
+
 export default class ColorfulShapeGraphic {
   private readonly canvas: HTMLCanvasElement;
 
@@ -8,7 +10,12 @@ export default class ColorfulShapeGraphic {
   public drawGreenRect() {
     const ctx = this.canvas.getContext('2d')!;
     ctx.fillStyle = 'green';
-    ctx.fillRect(10, 10, 150, 100);
-    console.log(ctx);
+    ctx.fillRect(...this.getCenterPosition([150, 100]), 150, 100);
+  }
+
+  private getCenterPosition(targetXY: VertexXY): VertexXY {
+    const {width, height} = this.canvas;
+    const [x, y] = targetXY;
+    return [width / 2 - x / 2, height / 2 - y / 2];
   }
 }
