f2aa25f818d025e05d20f7ce7d53bea1af7ef5a4
kjkandrea <kjkandrea@gmail.com>
2022-11-19 15:52:38 +0900

    feat: add click target validator

--- src/main.ts
+++ src/main.ts
@@ -8,9 +8,9 @@ rootElement.innerHTML = `
     <h1>Cypress, 테스트를 부탁해! 🧚</h1>
     <canvas></canvas>
     <div>
-      <button data-event='draw-green-rect'>초록 사각형</button>
-      <button data-event='draw-red-circle'>빨간 동그라미</button>
-      <button data-event='draw-blue-triangle'>파란 삼각형</button>
+      <button type="button" data-event='draw-green-rect'>초록 사각형</button>
+      <button type="button" data-event='draw-red-circle'>빨간 동그라미</button>
+      <button type="button" data-event='draw-blue-triangle'>파란 삼각형</button>
     </div>
   </div>
 `;
@@ -18,6 +18,26 @@ rootElement.innerHTML = `
 const canvasElement = rootElement.querySelector('canvas')!;
 
 const colorfulShapeGraphic = new ColorfulShapeGraphic(canvasElement);
+rootElement.addEventListener('click', ({target}) => {
+  if (!isEventButtonElement(target)) return;
+  const event = target.dataset.event;
+  console.log(event);
+});
 colorfulShapeGraphic.drawGreenRect();
 colorfulShapeGraphic.drawRedCircle();
 colorfulShapeGraphic.drawBlueTriangle();
+
+interface EventButtonElement extends HTMLButtonElement {
+  dataset: {
+    event: string;
+  };
+}
+
+function isEventButtonElement(
+  eventTarget: EventTarget | null
+): eventTarget is EventButtonElement {
+  if (eventTarget === null) return false;
+  const isButtonElement = eventTarget instanceof HTMLButtonElement;
+  if (!isButtonElement) return false;
+  return !!eventTarget.dataset?.event;
+}
