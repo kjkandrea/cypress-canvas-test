ca99183a7c71136c83bec734ce2b7cfa50f0307b
kjkandrea <kjkandrea@gmail.com>
2022-11-19 15:37:56 +0900

    feat: add control btn

--- src/main.ts
+++ src/main.ts
@@ -6,11 +6,16 @@ const rootElement = document.querySelector<HTMLDivElement>('#app')!;
 rootElement.innerHTML = `
   <div>
     <h1>Cypress, 테스트를 부탁해! 🧚</h1>
+    <canvas></canvas>
+    <div>
+      <button data-event='draw-green-rect'>초록 사각형</button>
+      <button data-event='draw-red-circle'>빨간 동그라미</button>
+      <button data-event='draw-blue-triangle'>파란 삼각형</button>
+    </div>
   </div>
 `;
 
-const canvasElement = document.createElement('canvas');
-rootElement.append(canvasElement);
+const canvasElement = rootElement.querySelector('canvas')!;
 
 const colorfulShapeGraphic = new ColorfulShapeGraphic(canvasElement);
 colorfulShapeGraphic.drawGreenRect();
