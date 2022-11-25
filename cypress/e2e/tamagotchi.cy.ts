82cad47e162c0272ec15a867f2f79a926db18dfd
kjkandrea <kjkandrea@gmail.com>
2022-11-25 23:34:26 +0900

    test: add poop test

--- cypress/e2e/tamagotchi.cy.ts
+++ cypress/e2e/tamagotchi.cy.ts
@@ -7,7 +7,6 @@ describe('다마고찌', () => {
 
   it('Start 버튼을 클릭하면 다마고찌가 렌더링된다.', () => {
     cy.contains('Start').click();
-
     cy.get('[data-test-id="screen"]')
       .should('have.css', 'width')
       .and('eq', '128px');
@@ -17,5 +16,15 @@ describe('다마고찌', () => {
       .and('eq', '128px');
 
     cy.get('button').contains('Clean Poop');
+  });
+
+  it('Start 이후 약 4초 후 Poop 이 생성된다.', () => {
+    cy.clock();
+    cy.contains('Start').click();
+    cy.tick(6050);
+    cy.get('.poop').should('be.visible');
+    cy.clock().then(clock => {
+      clock.restore();
+    });
   });
 });
