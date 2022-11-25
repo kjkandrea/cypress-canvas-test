08fc0d5c670059dc6b543eba8b4fe44c2479feb7
kjkandrea <kjkandrea@gmail.com>
2022-11-26 00:15:13 +0900

    test: add viewport

--- cypress/e2e/tamagotchi.cy.ts
+++ cypress/e2e/tamagotchi.cy.ts
@@ -2,6 +2,7 @@ export {};
 
 describe('다마고찌', () => {
   beforeEach(() => {
+    cy.viewport('iphone-x');
     cy.visit('http://localhost:5173');
   });
 
@@ -21,10 +22,26 @@ describe('다마고찌', () => {
   it('Start 이후 약 4초 후 Poop 이 생성된다.', () => {
     cy.clock();
     cy.contains('Start').click();
-    cy.tick(6050);
+    cy.tick(4050);
     cy.get('.poop').should('be.visible');
+    cy.get('.Margo').should('have.class', 'has-pooped');
     cy.clock().then(clock => {
       clock.restore();
     });
+  });
+
+  it('Poop 은 4초 마다 생성되고 Clean Poop 버튼을 누르면 Poop 이 지워진다.', () => {
+    cy.clock();
+    cy.contains('Start').click();
+
+    const makeAndClean = () => {
+      cy.tick(4050);
+      cy.contains('Clean Poop').click();
+      cy.get('.poop').should('be.hidden');
+    };
+
+    makeAndClean();
+    makeAndClean();
+    makeAndClean();
   });
 });
