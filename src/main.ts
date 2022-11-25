d46679cd90a934e9b3eb45b6d28b55372b6585a6
kjkandrea <kjkandrea@gmail.com>
2022-11-25 18:55:11 +0900

    feat: add funny poop tamagotchi

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,49 @@
-import './style.css';
+import './style.scss';
 
 const rootElement = document.querySelector<HTMLDivElement>('#app')!;
 
 rootElement.innerHTML = `
-  
+  <div class='tamagotchi'>
+    <div class='tamagotchi__inner'>
+      <div class='screen'>
+        <div class='screen__inner'>
+          <div class='Margo'></div>
+          <div class='poop'></div>
+        </div>
+      </div>
+    </div>
+    <button class='clean-poop'>Clean Poop</button>
+  </div>
 `;
+
+const Margo = document.querySelector('.Margo') as HTMLDivElement;
+const poopArea = document.querySelector('.poop') as HTMLDivElement;
+const cleanPoopButton = document.querySelector('.clean-poop') as HTMLDivElement;
+
+const poop = {
+  cleaned: 0,
+  show() {
+    if (!Margo.classList.contains('has-pooped')) {
+      poopArea.classList.add('is-visible');
+      Margo.classList.add('has-pooped');
+    }
+  },
+  hide() {
+    poopArea.classList.remove('is-visible');
+    Margo.classList.remove('has-pooped');
+  },
+  clean() {
+    if (Margo.classList.contains('has-pooped')) {
+      poop.hide();
+      poop.cleaned++;
+    }
+  },
+};
+
+setInterval(poop.show, 4000);
+
+const init = () => {
+  cleanPoopButton.addEventListener('click', poop.clean);
+};
+
+init();
