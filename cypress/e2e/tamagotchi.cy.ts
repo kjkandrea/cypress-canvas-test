2ac71b0942d4fe0ba06ad15c0841f61f3a27428c
kjkandrea <kjkandrea@gmail.com>
2022-12-03 11:22:00 +0900

    refactor: cy 체인 적극적으로 사용

--- cypress/e2e/tamagotchi.cy.ts
+++ cypress/e2e/tamagotchi.cy.ts
@@ -20,89 +20,99 @@ describe('다마고찌', () => {
 
   it(`Start 이후 약 ${ACTION_DURATION / 1000}초 후 Poop 이 생성된다.`, () => {
     cy.clock();
-    cy.contains('Start').click();
-    cy.tick(ACTION_DURATION);
-    cy.get('[data-test-id="poop"]').should('be.visible');
-    cy.get('[data-test-id="karenin"]').should('have.class', 'has-pooped');
-    cy.clock().then(clock => {
-      clock.restore();
-    });
+    cy.contains('Start')
+      .click()
+      .tick(ACTION_DURATION)
+      .get('[data-test-id="poop"]')
+      .should('be.visible')
+      .parent()
+      .get('[data-test-id="karenin"]')
+      .should('have.class', 'has-pooped');
   });
 
   it(`Poop 은 ${
     ACTION_DURATION / 1000
   }초 마다 생성되고 Clean Poop 버튼을 누르면 Poop 이 지워진다.`, () => {
     cy.clock();
-    cy.contains('Start').click();
-
-    cy.tick(ACTION_DURATION);
-    cy.contains('Clean Poop').click();
-    cy.get('[data-test-id="poop"]').should('be.hidden');
+    cy.contains('Start').click().tick(ACTION_DURATION);
+    cy.contains('Clean Poop')
+      .click()
+      .get('[data-test-id="poop"]')
+      .should('be.hidden');
   });
 
   it('생성된 Poop 을 치울 때 마다 Clean Count 가 증가한다.', () => {
     cy.clock();
-    cy.contains('Start').click();
-
-    cy.tick(ACTION_DURATION);
-    cy.contains('Clean Poop').click();
-    cy.contains('Clean Count : 1');
+    cy.contains('Start').click().tick(ACTION_DURATION);
+    cy.contains('Clean Poop').click().parent().contains('Clean Count : 1');
   });
 
   it(`Poop 을 치운 후 ${
     ACTION_DURATION / 1000
   }초 이후 Give a Meal 버튼이 활성화 된다.`, () => {
     cy.clock();
-    cy.contains('Start').click();
-    cy.tick(ACTION_DURATION);
-    cy.contains('Clean Poop').click();
-    cy.contains('Give a Meal').should('be.enabled');
+    cy.contains('Start').click().tick(ACTION_DURATION);
+    cy.contains('Clean Poop')
+      .click()
+      .parent()
+      .contains('Give a Meal')
+      .should('be.enabled');
   });
 
   it('활성화 된 Give a Meal 버튼을 클릭하면 Meal 이 나타난다.', () => {
     cy.clock();
-    cy.contains('Start').click();
-    cy.tick(ACTION_DURATION);
-    cy.contains('Clean Poop').click();
-    cy.contains('Give a Meal').click();
-    cy.get('[data-test-id="meal"]').should('be.visible');
+    cy.contains('Start').click().tick(ACTION_DURATION);
+    cy.contains('Clean Poop')
+      .click()
+      .parent()
+      .contains('Give a Meal')
+      .click()
+      .get('[data-test-id="meal"]')
+      .should('be.visible');
   });
 
   it('Meal 이 나타나면 4초 뒤 사라지고 Poop 이 나타난다.', () => {
     cy.clock();
-    cy.contains('Start').click();
-    cy.tick(ACTION_DURATION);
-    cy.contains('Clean Poop').click();
-    cy.contains('Give a Meal').click();
-    cy.get('[data-test-id="meal"]').should('be.visible');
-    cy.tick(ACTION_DURATION);
-    cy.get('[data-test-id="meal"]').should('be.hidden');
-    cy.tick(ACTION_DURATION);
-    cy.get('[data-test-id="poop"]').should('be.visible');
+    cy.contains('Start').click().tick(ACTION_DURATION);
+    cy.contains('Clean Poop')
+      .click()
+      .parent()
+      .contains('Give a Meal')
+      .click()
+      .get('[data-test-id="meal"]')
+      .should('be.visible')
+      .tick(ACTION_DURATION)
+      .get('[data-test-id="meal"]')
+      .should('be.hidden')
+      .tick(ACTION_DURATION)
+      .get('[data-test-id="poop"]')
+      .should('be.visible');
   });
 
   it('위와 같이 Karenin 은 영원히 순환하는 시간을 산다.', () => {
+    const howMany = range(99); // if Infinity, cypress dies.. 😔
+
     cy.clock();
-    cy.contains('Start').click();
+    cy.contains('Start').click().tick(ACTION_DURATION);
 
     const kareninLife = (i: number) => {
-      cy.tick(ACTION_DURATION);
-      cy.contains('Clean Poop').click();
-      cy.contains(`Clean Count : ${i}`);
-      cy.contains('Give a Meal').click();
-      cy.get('[data-test-id="meal"]').should('be.visible');
-      cy.tick(ACTION_DURATION);
-      cy.get('[data-test-id="meal"]').should('be.hidden');
-      cy.tick(ACTION_DURATION);
-      cy.get('[data-test-id="poop"]').should('be.visible');
+      cy.contains('Clean Poop').click().parent().contains(`Clean Count : ${i}`);
+      cy.contains('Give a Meal')
+        .click()
+        .get('[data-test-id="meal"]')
+        .should('be.visible')
+        .tick(ACTION_DURATION)
+        .get('[data-test-id="meal"]')
+        .should('be.hidden')
+        .tick(ACTION_DURATION)
+        .get('[data-test-id="poop"]')
+        .should('be.visible');
     };
 
-    const range = function* (stop: number) {
+    function* range(stop: number) {
       let i = -1;
       while (++i < stop) yield i;
-    };
-
-    const howMany = range(1); // if Infinity, cypress dies.. 😔
+    }
     let looping = true;
     while (looping) {
       const {value, done} = howMany.next();
