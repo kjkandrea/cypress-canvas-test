aa0c390cbc09f58bcd9ec67a57530b0e0b383efa
kjkandrea <kjkandrea@gmail.com>
2022-12-06 10:31:50 +0900

    refactor: context refactoring

--- cypress/e2e/tamagotchi.cy.ts
+++ cypress/e2e/tamagotchi.cy.ts
@@ -22,99 +22,112 @@ describe('다마고찌', () => {
       cy.getByTestId('karenin').toMatchImageSnapshot();
   });
 
-  it(`Start 이후 약 ${ACTION_DURATION / 1000}초 후 Poop 이 생성된다.`, () => {
-    cy.tamagotchiStart()
-      .tick(ACTION_DURATION)
-      .getByTestId('poop')
-      .should('be.visible')
-      .getByTestId('karenin')
-      .should('have.class', 'has-pooped');
+  describe(`Start 이후 약 ${ACTION_DURATION / 1000}초 후`, () => {
+    beforeEach(() => {
+      cy.tamagotchiStart().tick(ACTION_DURATION);
+    });
 
-    runSnapshotTest && cy.get('[data-test-id="poop"]').toMatchImageSnapshot();
-  });
-
-  it('Clean Poop 버튼을 누르면 Poop 이 지워진다.', () => {
-    cy.tamagotchiStart()
-      .tick(ACTION_DURATION)
-      .root()
-      .contains('Clean Poop')
-      .click()
-      .getByTestId('poop')
-      .should('be.hidden');
-  });
-
-  it('생성된 Poop 을 치울 때 마다 Clean Count 가 증가한다.', () => {
-    cy.tamagotchiStart()
-      .cleanPoop()
-      .root()
-      .contains('Clean Count : 1');
-  });
-
-  it(`Poop 을 치운 후 ${
-    ACTION_DURATION / 1000
-  }초 이후 Give a Meal 버튼이 활성화 된다.`, () => {
-    cy.tamagotchiStart()
-      .cleanPoop()
-      .root()
-      .contains('Give a Meal')
-      .should('be.enabled');
-  });
-
-  it('활성화 된 Give a Meal 버튼을 클릭하면 Meal 이 나타난다.', () => {
-    cy.tamagotchiStart()
-      .cleanPoop()
-      .contains('Give a Meal')
-      .click()
-      .getMeal()
-      .should('be.visible');
-
-    runSnapshotTest && cy.get('[data-test-id="meal"]').toMatchImageSnapshot();
-  });
-
-  it('Meal 이 나타나면 4초 뒤 사라지고 Poop 이 나타난다.', () => {
-    cy.tamagotchiStart()
-      .cleanPoop()
-      .giveMeal()
-      .tick(ACTION_DURATION)
-      .getMeal()
-      .should('be.hidden')
-      .tick(ACTION_DURATION)
-      .getPoop()
-      .should('be.visible');
-  });
-
-  it('위와 같이 Karenin 은 영원히 순환하는 시간을 산다.', () => {
-    const howMany = range(1); // if Infinity, cypress dies.. 😔
-
-    cy.tamagotchiStart()
-
-    const kareninLife = (i: number) => {
-      cy.cleanPoop()
-        .contains(`Clean Count : ${i}`)
-        .giveMeal()
-        .getMeal()
+    it(`Poop 이 생성된다.`, () => {
+      cy.getPoop()
         .should('be.visible')
+        .getByTestId('karenin')
+        .should('have.class', 'has-pooped');
+
+      runSnapshotTest && cy.get('[data-test-id="poop"]').toMatchImageSnapshot();
+    });
+
+    it('Clean Poop 버튼을 누르면 Poop 이 지워진다.', () => {
+      cy
+        .contains('Clean Poop')
+        .click()
+        .getByTestId('poop')
+        .should('be.hidden');
+    });
+
+    it('생성된 Poop 을 치울 때 마다 Clean Count 가 증가한다.', () => {
+      cy
+        .cleanPoop()
+        .contains('Clean Count : 1');
+    });
+  })
+
+  describe(`Start 이후 첫번째 Poop을 치움`, () => {
+    beforeEach(() => {
+      cy.tamagotchiStart().cleanPoop()
+    });
+
+    it(`Give a Meal 버튼이 활성화 된다.`, () => {
+      cy
+        .contains('Give a Meal')
+        .should('be.enabled');
+    });
+
+    it('활성화 된 Give a Meal 버튼을 클릭하면 Meal 이 나타난다.', () => {
+      cy
+        .contains('Give a Meal')
+        .click()
+        .getMeal()
+        .should('be.visible');
+
+      runSnapshotTest && cy.get('[data-test-id="meal"]').toMatchImageSnapshot();
+    });
+
+    it('Meal 이 나타나면 4초 뒤 karenin 이 먹는다.', () => {
+      cy
+        .giveMeal()
+        .tick(ACTION_DURATION)
+        .getMeal()
+        .should('be.hidden')
+    });
+
+    it('Meal 을 먹으면 Poop 이 나타난다.', () => {
+      cy
+        .giveMeal()
         .tick(ACTION_DURATION)
         .getMeal()
         .should('be.hidden')
         .tick(ACTION_DURATION)
         .getPoop()
         .should('be.visible');
-    };
+    });
+  });
 
-    function* range(stop: number) {
-      let i = -1;
-      while (++i < stop) yield i;
-    }
-    let looping = true;
-    while (looping) {
-      const {value, done} = howMany.next();
-      if (done) {
-        looping = false;
-        break;
+  describe('루프 테스트', () => {
+    beforeEach(() => {
+      cy.tamagotchiStart()
+    });
+
+    it('위와 같이 Karenin 은 영원히 순환하는 시간을 산다.', () => {
+      const howMany = range(10); // if Infinity, cypress dies.. 😔
+
+      const kareninLife = (i: number) => {
+        cy.cleanPoop()
+          .contains(`Clean Count : ${i}`)
+          .giveMeal()
+          .getMeal()
+          .should('be.visible')
+          .tick(ACTION_DURATION)
+          .getMeal()
+          .should('be.hidden')
+          .tick(ACTION_DURATION)
+          .getPoop()
+          .should('be.visible');
+      };
+
+      function* range(stop: number) {
+        let i = -1;
+        while (++i < stop) yield i;
       }
+      let looping = true;
+      while (looping) {
+        const {value, done} = howMany.next();
+        if (done) {
+          looping = false;
+          break;
+        }
 
-      if (typeof value === 'number') kareninLife(value + 1);
-    }
+        if (typeof value === 'number') kareninLife(value + 1);
+      }
+    });
   });
 });
